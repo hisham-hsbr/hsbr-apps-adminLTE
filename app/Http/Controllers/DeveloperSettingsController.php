cfb0df72cfd0ab8956fcff5aed6450e48c310aea
hisham.hsbr <hisham.hsbr@gmail.com>
2023-10-07 03:47:02 -0400

    add default masters-crm

---
+++ app/Http/Controllers/DeveloperSettingsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DeveloperSettings;
+use Illuminate\Http\Request;
+
+class DeveloperSettingsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(DeveloperSettings $developerSettings)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(DeveloperSettings $developerSettings)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, DeveloperSettings $developerSettings)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(DeveloperSettings $developerSettings)
+    {
+        //
+    }
+}
